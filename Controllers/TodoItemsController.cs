e4e7a4570a194ac051de7dce80661f8f03ca1576
Kevin <Kevin22pr@gmail.com>
2020-10-06 20:05:00 -0400

    UP: Updated routes

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 namespace Todo_MVC_API.Controllers {
-    [Route("api/[TodoItems]")]
+    [Route("api/todoItems")]
     [ApiController]
     public class TodoItemsController : ControllerBase {
         private readonly TodoContext _context;
--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -3,11 +3,9 @@ using Microsoft.EntityFrameworkCore;
 namespace TodoApi.Models {
     public class TodoContext : DbContext {
         public TodoContext(DbContextOptions<TodoContext> options) : base(options) {
-
+            
         }
 
         public DbSet<TodoItem> TodoItems { get; set; }
-
-
     }
 }
